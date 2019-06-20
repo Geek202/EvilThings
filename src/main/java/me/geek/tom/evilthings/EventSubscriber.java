35d1df05fcc047021fdb43cc682a38efec2e2941
Tom_The_Geek <tgbryan39@gmail.com>
2019-06-20 15:54:48 +0100

    VERSION 1.0.1-SNAPSHOT Notes: - Fixed all known bugs! - Moved block and item registering to the EventSubscriber from the Proxy (Thanks suppergerrie2!)

--- src/main/java/me/geek/tom/evilthings/EventSubscriber.java
+++ src/main/java/me/geek/tom/evilthings/EventSubscriber.java
@@ -1,7 +1,10 @@
 package me.geek.tom.evilthings;
 
+import me.geek.tom.evilthings.blocks.BlockBasic;
 import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -11,14 +14,22 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber(modid = Reference.MODID)
 public class EventSubscriber {
+
+    public static Item testItem = new Item().setRegistryName("green_crystal").setUnlocalizedName("green_crystal");
+    public static Block testBlock = new BlockBasic("green_crystal_ore", Material.ROCK);
+    public static Item testItemBlock;
+
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
-        EvilThings.proxy.registerItems(event);
+        // System.out.println("register items!");
+        event.getRegistry().registerAll(testItem);
+        testItemBlock = new ItemBlock(testBlock).setRegistryName("green_crystal_ore");
+        event.getRegistry().register(testItemBlock);
     }
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event) {
-        EvilThings.proxy.registerBlocks(event);
+        event.getRegistry().register(testBlock);
     }
 
     @SideOnly(Side.CLIENT)
